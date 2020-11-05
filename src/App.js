bcd30917ab9e79d557fc063f0691cd9efaa0b051
Gareth Whitley <me@garethwhitley.online>
2020-11-05 11:50:41 +0000

    changed button component to a functional component with props and the App component as a class component with state that bubbles to the button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
+import React from 'react';
+
 import './App.css';
 
 import LikeButton from './components/like-button/like-button.component';
 
-function App() {
-  return (
-    <div>
-      <LikeButton />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      liked: false,
+      checkMark: ''
+    }
+  }
+
+  
+
+  toggleLiked = () => {
+    let localLiked = this.state.liked;
+    localLiked = !localLiked;
+
+    this.setState({liked: localLiked});
+
+    let localCheckMark = '✓';
+    if (localLiked) {
+      this.setState({checkMark: localCheckMark});
+    } else {
+      this.setState({checkMark: ''});
+    }
+
+    
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div>
+        <LikeButton 
+          checkMark={this.state.checkMark}
+          toggle={this.toggleLiked}
+        />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
