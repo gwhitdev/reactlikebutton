c491f414c0847b25fd0ef1ccef1d412ce01714b1
Gareth Whitley <me@garethwhitley.online>
2020-11-05 11:53:40 +0000

    added comments

--- src/App.js
+++ src/App.js
@@ -15,25 +15,35 @@ class App extends React.Component {
   }
 
   
-
+  // create like button toggle method
   toggleLiked = () => {
+
+    // create local liked variable to hold the liked state
     let localLiked = this.state.liked;
     localLiked = !localLiked;
 
     this.setState({liked: localLiked});
 
+    // create local checkmark variable to hold the character for setting later
     let localCheckMark = '✓';
+
+    // create logic to check the status of having liked something
     if (localLiked) {
+      // if localLiked === true then set this.state.checkMark to localCheckMark
       this.setState({checkMark: localCheckMark});
     } else {
+      // if localLiked === false, set checkMark back to an empty string
       this.setState({checkMark: ''});
     }
 
     
   }
-
+  // render component
   render() {
+
+    // console.log the state to see what's going on
     console.log(this.state)
+
     return (
       <div>
         <LikeButton 
