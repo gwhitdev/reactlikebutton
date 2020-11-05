e26f0a9d62930d80b4c270646e7f752b2693498a
Gareth Whitley <me@garethwhitley.online>
2020-11-05 11:57:45 +0000

    updated comments

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
     // flip the state
     localLiked = !localLiked;
 
+    // set the now flipped state
     this.setState({liked: localLiked});
 
     // create local checkmark variable to hold the character for setting later
